93719028620b177b9e818c95a2948f2b770697a4
GitHub Action <action@github.com>
2021-09-27 06:26:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/pickupstar/wxread/MyBundle.kt
+++ src/main/kotlin/com/github/pickupstar/wxread/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.pickupstar.wxread
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/pickupstar/wxread/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pickupstar/wxread/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.pickupstar.wxread.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pickupstar.wxread.MyBundle
 
 class MyProjectService(project: Project) {
 
